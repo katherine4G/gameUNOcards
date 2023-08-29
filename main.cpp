c9dd702488d926697e3ff7800c18ac1df2ab346d
katherine4G <112660945+katherine4G@users.noreply.github.com>
2023-08-29 17:22:44 -0600

    window and background

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+//#include <stdio.h>
 #include <SFML/Graphics.hpp>
 //#include <SFML/Audio.hpp>
 using namespace std;
 using namespace sf;
 
+int main() {
+	//window
+	RenderWindow window(VideoMode(1444, 802), "UNO card game");
+	window.setFramerateLimit(60); //frames per second
 
+	//background
+	Texture backgTexture;
+	Sprite backg;
+	if (backgTexture.loadFromFile("fondoMenu.png")) cout<<"Error al cragar la textura de fondo"<<endl;
+		backg.setTexture(backgTexture);
+	
+
+	while (window.isOpen()) 
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				window.close();
+		}
+	}
+	window.clear();
+	window.draw(backg);
+	window.display();
+	return 0;
+}
